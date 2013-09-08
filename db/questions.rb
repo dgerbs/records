ca1dc12d7520893ce26f904ff55274928faf86cb
Doug Gerber <dojoh@outlook.com>
2013-09-07 21:08:26 -0400

    Complete ActiveRecord query

--- db/questions.rb
+++ db/questions.rb
@@ -1,10 +1,10 @@
 # What 3 towns have the highest population of citizens that are 65 years and older?
-TownHealthRecord.select('name', 'aged_sixtyfive_plus').where.not('Massachusetts Total').order('aged_sixtyfive_plus DESC').limit(3)
+TownHealthRecord.select('name, aged_sixtyfive_plus').where.not(name: 'Massachusetts Total').order('aged_sixtyfive_plus DESC').first(3)
 # What 3 towns have the highest population of citizens that are 19 years and younger?
-TownHealthRecord.select('name', 'aged_zero_to_nineteen').where.not('Massachusetts Total').order('aged_zero_to_nineteen DESC').limit(3)
+TownHealthRecord.select('name', 'aged_zero_to_nineteen').where.not(name: 'Massachusetts Total').order('aged_zero_to_nineteen DESC').limit(3)
 # What 5 towns have the lowest per capita income?
-TownHealthRecord.select('name', 'per_capita_income').where.not('Massachusetts Total').order('per_capita_income ASC').limit(5)
+TownHealthRecord.select('name', 'per_capita_income').where.not(name: 'Massachusetts Total').order('per_capita_income ASC').limit(5)
 # Omitting Boston, Becket, and Beverly, what town has the highest percentage of teen births?
-TownHealthRecord.select('name', 'teen_birth_rate').where.not('Massachusetts Total', 'Boston', 'Becket', 'Beverly').order('teen_birth_rate DESC').limit(1)
+TownHealthRecord.select('name', 'teen_birth_rate').where.not(name: ['Massachusetts Total', 'Boston', 'Becket', 'Beverly']).order('teen_birth_rate DESC').limit(1)
 # Omitting Boston, what town has the highest number of infant mortalities?
-TownHealthRecord.select('name', 'infant_mortality_rate').where.not('Massachusetts Total', 'Boston').order('infant_mortality_rate DESC').limit(1)
+TownHealthRecord.select('name', 'infant_mortality_rate').where.not(name: ['Massachusetts Total', 'Boston']).order('infant_mortality_rate DESC').limit(1)
