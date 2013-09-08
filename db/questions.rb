ec463feb147aad69d21bd6c4106a1d5eda875796
Doug Gerber <dojoh@outlook.com>
2013-09-07 20:32:41 -0400

    Complete SQL query

--- db/questions.rb
+++ db/questions.rb
@@ -1,9 +1,10 @@
 # What 3 towns have the highest population of citizens that are 65 years and older?
-
+TownHealthRecord.select('name', 'aged_sixtyfive_plus').where('').order('name DESC').limit(3)
 # What 3 towns have the highest population of citizens that are 19 years and younger?
-
+TownHealthRecord.select('name', 'aged_zero_to_nineteen').where.('').order('name DESC').limit(3)
 # What 5 towns have the lowest per capita income?
-
+TownHealthRecord.select('name', 'per_capita_income').where('').order('name DESC').limit(5)
 # Omitting Boston, Becket, and Beverly, what town has the highest percentage of teen births?
-
+TownHealthRecord.select('name', 'teen_birth_rate').where.not('').order
 # Omitting Boston, what town has the highest number of infant mortalities?
+TownHealthRecord.select('name', 'infant_mortality_rate').where.not('').order
